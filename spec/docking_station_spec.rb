3c029135abce2b9aef8ed79efff6c3d902c854a0
Albert <ayanit1@googlemail.com>
2017-02-15 12:57:04 +0000

    Made changes to specifications for docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,12 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'return a new instance of bike class' do
+    expect(subject.release_bike).to be_a Bike
+  end
+
+  it 'expects bike to be working' do
+    expect(subject.release_bike.working?).to be true
+  end
 end

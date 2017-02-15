a487ae5a2259868bd129bd3216d960bab0d6bdf0
Albert <ayanit1@googlemail.com>
2017-02-15 18:24:35 +0000

    Add docking counter feature

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,26 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike, :bike_counter
+
+  def initialize
+    @bike_counter = true
+  end
 
   def release_bike
-    @bike = Bike.new
+    if @bike_counter == true
+      @bike = Bike.new
+      @bike_counter = false
+      return @bike
+    else
+      raise "error"
+    end
   end
 
   def dock(bike)
     @bike = bike
+    @bike_counter = true
+    return @bike
   end
 
 end
